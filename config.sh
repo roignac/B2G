7d43291e41b2ebd31d4f1c5b285cd25c707e1d02
Daz Jones <yuki@thebronasium.com>
2013-01-30 20:32:49 +0000

    Use my b2g-manifest

--- config.sh
+++ config.sh
@@ -28,7 +28,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/Dazzozo/b2g-manifest"}
 BRANCH=${BRANCH:-v1-train}
 
 GIT_TEMP_REPO="tmp_manifest_repo"
