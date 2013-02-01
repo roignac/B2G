40a81811c4d76f52f148ae5be38b801627041a48
Vadim Rutkovsky <roignac@gmail.com>
2013-02-01 23:52:25 +0100

    Switch to my manifest repo

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/Dazzozo/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/roignac/b2g-manifest"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
